83888912175982149167422c7c496be2c034454b
Ryan Mello <ryanmello897@gmail.com>
2025-09-03 19:43:22 -0700

    take in a user prompt

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    
+    if len(sys.argv) < 2:
+        print("I need a prompt")
+        sys.exit(1)
+    
+    prompt = sys.argv[1]
+    print(f"Prompt: {prompt}")
 
-client = genai.Client(api_key=api_key)
+    client = genai.Client(api_key=api_key)
 
-res = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents="Why is the sky blue?"
-)
+    res = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents=prompt
+    )
 
-print(res.text)
+    print(res.text)
+
+if __name__ == "__main__":
+    main()
+    
