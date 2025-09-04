6bd58a43e756159d09946818678f27fb742c8095
Ryan Mello <ryanmello897@gmail.com>
2025-09-03 19:36:39 -0700

    make gemini call

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def main():
-    print("Hello from cipher!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+res = client.models.generate_content(
+    model="gemini-2.0-flash-001", contents="Why is the sky blue?"
+)
+
+print(res.text)
